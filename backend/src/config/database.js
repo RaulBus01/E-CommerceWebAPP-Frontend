d5fefc4c610b85ef4da13fbb38431e3fb293ab62
RaulBus <raul.bus@student.upt.ro>
2024-07-11 21:36:07 +0300

    Repair Backend & Frontend

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-
+console.log(process.env.DB_URL);
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
