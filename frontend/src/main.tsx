fd85506da31f572c0f87cf76a3c6f4f4026109e9
RaulBus <raul.bus@student.upt.ro>
2024-07-24 18:27:07 +0300

    Setup Main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,14 +4,17 @@ import App from './App.tsx'
 import {BrowserRouter} from 'react-router-dom'
 import Navbar from './navbar/Navbar.tsx'
 import Footer from './footer/Footer.tsx'
-import './index.css'
+import './theme.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Navbar />
-        <App />
-      <Footer />
-    </BrowserRouter>
+
+      <BrowserRouter>
+        <Navbar />
+          <App />
+        <Footer />
+      </BrowserRouter>
+
   </React.StrictMode>,
 )
--- frontend/src/pages/home-page/home.jsx
+++ frontend/src/pages/home-page/home.jsx
@@ -4,8 +4,10 @@ import Dropdown from "../../components/dropdown/dropdown";
 
 const Home = () => {
   return (
-    <div className="home">
-      <h1>EmagoAmazoniana</h1>
+    <div className="home" 
+   
+    >
+      <h1 >EmagoAmazoniana</h1>
     </div>
   );
 };
