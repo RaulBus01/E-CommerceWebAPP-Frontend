2494a6551cdffd0279be20e5bd1935cde1b89e6c
RaulBus <raul.bus@student.upt.ro>
2024-07-28 10:51:47 +0300

    Side-Menu fixed

--- frontend/src/components/side-menu/sideMenu.tsx
+++ frontend/src/components/side-menu/sideMenu.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import  useUser  from "../../hooks/useUser";
+import { useAuth } from "../../hooks/useAuth";
 import './sideMenu.css'
 
 const SideMenu = ({setSelectedMenu}) => {
     const [selected, setSelected] = useState<string>('Personal information');
+    const {userId, token} = useAuth();
+    const {user, loading} = useUser(userId, token);
 
     const handleSelectedButton = (buttonName: string) => {
         setSelected(buttonName);
@@ -12,7 +16,7 @@ const SideMenu = ({setSelectedMenu}) => {
     return(
         <>
             <div className="main-wrapper">
-                <h3>Vlad Cirlugea</h3>
+                <h3>Hello, {user?.first_name} {user?.last_name}</h3>
                 <ul>
                     <li>
                         <button className={selected === 'Personal information' ? 'selected' : ''}
