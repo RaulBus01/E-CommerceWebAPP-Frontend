fec7a279a81aafd0fffbeff5a4b68cc70c53dccd
RaulBus <raul.bus@student.upt.ro>
2024-08-05 14:07:01 +0300

    navbar fix

--- frontend/src/navbar/Navbar.tsx
+++ frontend/src/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink, useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import SearchBar from "../components/controls/searchbar/searchBar";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
@@ -7,7 +7,6 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { useAuth } from "../hooks/useAuth";
 import Dropdown from "../components/dropdown/dropdown";
-
 
 const Navbar = () => {
   const { isAuthenticated, user } = useAuth();
@@ -18,34 +17,31 @@ const Navbar = () => {
         <ul>
           <li>
             <NavLink to="/">
-              <img className="logo" src="src\assets\Logo.png" alt="logo" />
+              <img className="logo" src="src/assets/Logo.png" alt="logo" />
             </NavLink>
           </li>
           <li className="search-bar-container">
             <SearchBar />
           </li>
           <div className="icons">
-          <li className="nav-icons">
-            <NavLink
-              to={isAuthenticated ? `/user-dashboard/${user?.id}` : "/login"}
-            >
-              <AccountCircleIcon />
-              <span className="nav-text">Account</span>
-            </NavLink>
-          </li>
-          
-          <li className="nav-icons">
-            <NavLink to="/favorites">
-              <FavoriteBorderIcon />
-              <span className="nav-text">Favourites</span>
-            </NavLink>
-          </li>
-          <li className="nav-icons">
-            <NavLink to="/cart">
-              <ShoppingCartIcon />
-              <span className="nav-text">Cart</span>
-            </NavLink>
-          </li>
+            <li className="nav-icons">
+              <NavLink to={isAuthenticated ? `/user-dashboard/${user?.id}` : "/login"}>
+                <AccountCircleIcon />
+                <span className="nav-text">Account</span>
+              </NavLink>
+            </li>
+            <li className="nav-icons">
+              <NavLink to="/favorites">
+                <FavoriteBorderIcon />
+                <span className="nav-text">Favourites</span>
+              </NavLink>
+            </li>
+            <li className="nav-icons">
+              <NavLink to="/cart">
+                <ShoppingCartIcon />
+                <span className="nav-text">Cart</span>
+              </NavLink>
+            </li>
           </div>
         </ul>
       </nav>
@@ -57,6 +53,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-
-//☰
